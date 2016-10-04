8c365af3568b01ab6a64a5b5cbaa37ee375aca9c
Syntaxious <SynPlaysMC@gmail.com>
2016-10-04 18:47:08 +0100

    Create EventsListener.php

---
+++ src/MMORPG/events/EventsListener.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace MMORPG\events;
+
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use pocketmine\event\player\PlayerItemConsumeEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityRegainHealthEvent;
+
+class EventsListener implements Listener {
+    
+    private $plugin;
+    
+    public function _construct(Main $plugin) {
+        $this->plugin = $plugin;
+        $this->server = $plugin->getServer();
+    }
+    
+    public function getServer() {
+        return $this->getServer();
+    }
+    
+    public function getPlugin() {
+        return $this->plugin;
+    }
+    
+
+}
+
+
